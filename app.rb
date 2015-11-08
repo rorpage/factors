0dcaf45ad622e330f9baaafa427c9d1c6bc5ea17
Robbie Page <robbie@clustertruck.com>
2015-11-08 15:47:43 -0500

    cleaned up the code

--- app.rb
+++ app.rb
@@ -15,14 +15,7 @@ get '/' do
 end
 
 get '/api?:q?' do
-  factors = []
-
-  if params['q']
-    factors = params['q'].to_i.factors
-  else
-    factors = Time.now.year.factors
-  end
-
+  factors = params['q'] ? params['q'].to_i.factors : Time.now.year.factors
   content_type :json
   { factors: factors }.to_json
 end
